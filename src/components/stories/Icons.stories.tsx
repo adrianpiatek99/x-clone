d28bd1eac87cde91d9fd07a4b65522cb26a0edbd
Adrian Piątek <adr654231@gmail.com>
2024-12-13 19:30:53 +0100

    feat: add icons story

---
+++ src/components/stories/Icons.stories.tsx
@@ -0,0 +1,23 @@
+import { createElement } from 'react';
+
+import * as icons from '@/icons';
+import type { Meta } from '@storybook/react';
+
+const meta = {
+  title: 'Design System / Icons',
+} satisfies Meta;
+
+export default meta;
+
+export const Icons = () => (
+  <div className='mx-auto flex w-full max-w-[800px] flex-wrap justify-center gap-4'>
+    {Object.keys(icons).map((key) => (
+      <div key={key} className='flex w-max flex-col items-center gap-2'>
+        {createElement(icons[key as keyof typeof icons], {
+          className: 'size-[36px]',
+        })}
+        <span className='w-[100px] truncate text-center'>{key}</span>
+      </div>
+    ))}
+  </div>
+);
