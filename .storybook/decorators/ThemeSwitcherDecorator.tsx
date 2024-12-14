acf2eaed6efff5e38285171e57bc4530c014008d
Adrian Piątek <adr654231@gmail.com>
2024-12-14 17:50:04 +0100

    fix: update styles in ThemeSwitcherDecorator

--- .storybook/decorators/ThemeSwitcherDecorator.tsx
+++ .storybook/decorators/ThemeSwitcherDecorator.tsx
@@ -7,22 +7,25 @@ const ThemeSwitcherDecorator = () => {
   const { theme: selectedTheme, setTheme } = useTheme();
 
   return (
-    <div className='flex flex-wrap gap-3'>
-      <span>Themes:</span>
-      {themeOptions.map(({ theme }) => (
-        <div key={theme} className='mb-3 flex gap-1'>
-          <input
-            type='radio'
-            id={theme}
-            name='theme'
-            value={theme}
-            checked={selectedTheme === theme}
-            onChange={() => setTheme(theme)}
-          />
-          <label htmlFor={theme}>{theme}</label>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className='flex flex-wrap gap-3'>
+        <span>Themes:</span>
+        {themeOptions.map(({ theme }) => (
+          <div key={theme} className='inline-flex items-center'>
+            <input
+              type='radio'
+              id={theme}
+              name='theme'
+              value={theme}
+              checked={selectedTheme === theme}
+              onChange={() => setTheme(theme)}
+            />{' '}
+            <label htmlFor={theme}>{theme}</label>
+          </div>
+        ))}
+      </div>
+      <br />
+    </>
   );
 };
 
