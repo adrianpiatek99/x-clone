ce6684a7a4184f6d66e4b6e3f12401f2cb7ac670
Adrian Piątek <adr654231@gmail.com>
2024-12-23 18:34:14 +0100

    feat: Add Tabs component with stories and debounce utility

---
+++ src/components/molecules/Tabs/Tabs.stories.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import { Box } from '@/components/atoms/Box';
+import type { Meta, StoryFn } from '@storybook/react';
+
+import { Tab } from './Tab';
+import type { TabsProps } from './Tabs';
+import Tabs from './Tabs';
+
+const meta = {
+  title: 'Components / Atoms / Tabs',
+  component: Tabs,
+  decorators: [(Story) => <Box className='w-fit'>{Story()}</Box>],
+} satisfies Meta<TabsProps>;
+
+export default meta;
+
+const Template: StoryFn<TabsProps> = () => {
+  const [currentTab, setCurrentTab] = useState('tab1');
+
+  return (
+    <Tabs value={currentTab} onChange={(tab) => setCurrentTab(tab)}>
+      <Tab value='tab1'>Tab 1</Tab>
+      <Tab value='tab2'>Tab 2</Tab>
+      <Tab value='tab3'>Tab 3</Tab>
+      <Tab value='tab4'>Tab 4</Tab>
+      <Tab value='tab5'>Tab 5</Tab>
+      <Tab value='tab6'>Long tab text text text text</Tab>
+    </Tabs>
+  );
+};
+
+export const Playground = Template.bind({});
---
+++ src/components/molecules/Tabs/Tabs.tsx
@@ -0,0 +1,115 @@
+import type { FC, ReactNode } from 'react';
+import React, {
+  Children,
+  cloneElement,
+  isValidElement,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+
+import { debounce } from '@/utils/debounce';
+
+import type { TabProps } from './Tab';
+
+type TabChildProps = { value?: string };
+
+type TabsIndicatorPosition = {
+  left: number;
+  width: number;
+};
+
+export type TabsProps = {
+  children: ReactNode;
+  value: string;
+  onChange?: (tab: string) => void;
+};
+
+const Tabs: FC<TabsProps> = ({ children, value, onChange }) => {
+  const tabGroupRef = useRef<HTMLDivElement>(null);
+  const [indicatorPosition, setIndicatorPosition] = useState<TabsIndicatorPosition>({
+    left: 0,
+    width: 0,
+  });
+
+  const childrenWithProps = useMemo(
+    () =>
+      Children.map(children, (child) => {
+        if (isValidElement(child)) {
+          const childValue = (child.props as TabChildProps).value;
+          const selected = childValue === value;
+
+          return cloneElement(child, {
+            selected,
+            onClick: () => onChange?.(childValue || ''),
+          } as TabProps);
+        }
+
+        return null;
+      }),
+    [children, value, onChange]
+  );
+
+  useEffect(() => {
+    const current = tabGroupRef.current;
+
+    if (current) {
+      const calcIndicatorListener = () => {
+        const selectedTabElement = current.querySelector(
+          '[aria-selected="true"]'
+        ) as HTMLElement | null;
+
+        if (selectedTabElement) {
+          const left = selectedTabElement.offsetLeft;
+          const width = selectedTabElement.offsetWidth;
+
+          selectedTabElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'center',
+          });
+
+          setIndicatorPosition({
+            left,
+            width,
+          });
+        }
+      };
+
+      const debouncedCalc = debounce(calcIndicatorListener, 250);
+
+      calcIndicatorListener();
+
+      window.addEventListener('resize', debouncedCalc);
+
+      return () => window.removeEventListener('resize', debouncedCalc);
+    }
+  }, [value]);
+
+  return (
+    <div className='flex h-[53px] shrink-0 overflow-hidden border-b border-border-1'>
+      <div className='hide-scrollbar relative flex grow overflow-x-auto overflow-y-hidden whitespace-nowrap'>
+        <div
+          ref={tabGroupRef}
+          className='relative flex w-full'
+          role='tablist'
+          aria-labelledby='tabs'
+        >
+          {childrenWithProps}
+        </div>
+        {!!indicatorPosition.width && (
+          <span
+            className='absolute bottom-0 flex h-[3px] w-[70px] rounded-md bg-primary delay-[width] duration-150'
+            style={{
+              width: `${indicatorPosition.width}px`,
+              left: `${indicatorPosition.left}px`,
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
