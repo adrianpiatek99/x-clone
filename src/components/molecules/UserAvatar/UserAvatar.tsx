86dbaeceeb0467886f1d87418423c1458c086dc9
Adrian Piątek <adr654231@gmail.com>
2025-04-08 15:56:46 +0200

    feat: Add UserAvatar component for displaying user profile images

---
+++ src/components/molecules/UserAvatar/UserAvatar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Avatar } from '@/components/atoms';
+import { ROUTES } from '@/constants/routes';
+import { useAppSession } from '@/hooks/useAppSession';
+
+const UserAvatar = () => {
+  const { user } = useAppSession();
+
+  if (!user) return null;
+
+  return (
+    <Avatar
+      src={user.profileImageUrl}
+      screenName={user.screenName}
+      href={ROUTES.PROFILE.DETAILS(user.screenName)}
+    />
+  );
+};
+
+export default UserAvatar;
