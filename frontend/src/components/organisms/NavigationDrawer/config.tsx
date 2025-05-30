4062170b76f78d7260e94fd03e46624cd065c51d
Adrian Piątek <adr654231@gmail.com>
2025-05-30 19:41:32 +0200

    refactor: Update active state logic for profile navigation in NavigationDrawer and SidebarMenu components to improve accuracy and maintainability

--- frontend/src/components/organisms/NavigationDrawer/config.tsx
+++ frontend/src/components/organisms/NavigationDrawer/config.tsx
@@ -28,7 +28,11 @@ export const navigationDrawerItems = ({
     {
       text: t('navigation.profile'),
       href: user ? ROUTES.PROFILE.DETAILS(user.screenName) : '',
-      active: user ? pathname.includes(ROUTES.PROFILE.DETAILS(user.screenName)) : false,
+      active: (() => {
+        if (!user) return false;
+
+        return Object.values(ROUTES.PROFILE).some((route) => pathname === route(user.screenName));
+      })(),
       icon: <Icon name='PersonOutlinedIcon' />,
       activeIcon: <Icon name='PersonIcon' />,
       visible: !!user,
